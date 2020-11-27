6aff7070a46c6cbd1b23b934f29b51c70e707256
bakuchi- <helveticaf0015se@gmail.com>
2020-11-27 19:32:51 +0900

    git rm

--- src/arbitrage/arbitrage.ts
+++ src/arbitrage/arbitrage.ts
@@ -57,7 +57,7 @@ const expectedReturn = (tickers: Tickers, arbitrageConfig): ArbitrageSet => {
         },
         totalMoney: function () { return this.buy * this.quantity },
         profit: function () {
-            return this.quantity * (this.diffPercent() * this.buy - this.tradeFeePercent * this.sellBasedJPY) / 100 - this.sendFeeCrypto * this.buy
+            return this.quantity * (Math.abs(this.diffPercent()) * this.buy - this.tradeFeePercent * this.sellBasedJPY) / 100 - this.sendFeeCrypto * this.buy
         },
         expectedReturn: function () {
             return 100 * this.profit() / this.totalMoney()
@@ -75,9 +75,12 @@ const expectedReturn = (tickers: Tickers, arbitrageConfig): ArbitrageSet => {
         //     value["sellBasedUSD"] = value["bid"] / value['rate']
         //     value["sellBasedJPY"] = value["bid"]
         // }
-        value["buy"] = value["cask"] > value["bbid"] ? value["bbid"] : null
-        value["sellBasedUSD"] = value["cbid"] > value["bbid"] ? value["bid"] : null
-        value["sellBasedJPY"] = value["cask"] > value["bbid"] ? value["cbid"] : null
+        value["buy"] = value["bbid"]
+        value["sellBasedUSD"] = value["bid"]
+        value["sellBasedJPY"] = value["cbid"]
+        // value["buy"] = value["cask"] > value["bbid"] ? value["bbid"] : null
+        // value["sellBasedUSD"] = value["cbid"] > value["bbid"] ? value["bid"] : null
+        // value["sellBasedJPY"] = value["cask"] > value["bbid"] ? value["cbid"] : null
         value["quantity"] = arbitrageConfig[key]["fixedSize"]
             ? parseFloat(arbitrageConfig[key]["size"]) / value["bbid"]
             : parseFloat(arbitrageConfig[key]["quantity"]);
@@ -88,8 +91,7 @@ const expectedReturn = (tickers: Tickers, arbitrageConfig): ArbitrageSet => {
     return tickers as ArbitrageSet
 }
 
-const logger = async (dataset: ArbitrageSet) => {
-    const messages: Message[] = [];
+const logger = (dataset: ArbitrageSet) => {
     for (const key in dataset) {
         if (Object.prototype.hasOwnProperty.call(dataset, key)) {
             const el = dataset[key];
@@ -109,23 +111,38 @@ const logger = async (dataset: ArbitrageSet) => {
     `
             } as Message;
             console.log(message['text']);
-            messages.push(message);
         }
     }
-    await pushMessage(axiosBase, messages)
 }
 
 const judgeOp = async (basis = 1, dataset: ArbitrageSet, log: Boolean): Promise<ArbitrageCalculator> => {
     let tmp = { symbol: '', bestReturn: 0 };
-    log && await logger(dataset);
     for (const [key, data] of Object.entries(dataset)) {
         tmp = (data.expectedReturn() > (tmp?.bestReturn || 0)) && { symbol: key, bestReturn: data.expectedReturn() }
     }
-    if (tmp.bestReturn > basis) {
-        await logger({ symbol: dataset[tmp.symbol] })
+    if ((tmp.bestReturn > basis) && !tmp?.bestReturn) {
+        log && logger(dataset);
+        const el = dataset[tmp.symbol];
+        const message = {
+            type: "text",
+            text: `
+    symbol > ${el.symbol}
+    Buy  [bitbank] ¥ > ${el.buy}
+    Sell [FTX] $ > ${el.sellBasedUSD}
+    Sell [FTX] ¥ > ${el.sellBasedJPY?.toFixed(2)}
+    割高 (bitbank比) % > ${el.diffPercent().toFixed(3)}
+    裁定金額 ¥ > ${el.totalMoney().toFixed(1)}
+    取引量 > ${el.quantity.toFixed(2)}
+    profit ¥ > ${el.profit().toFixed(1)}
+    expectedReturn % > ${el.expectedReturn().toFixed(3)}
+    送金手数料 ¥ > ${el.sendFeeJPY().toFixed(0)}
+    `
+        } as Message;
+        await pushMessage(axiosBase, [message]);
         return dataset[tmp.symbol]
-    };
+    }
     return null;
+
 }
 
 (async () => {
@@ -134,6 +151,7 @@ const judgeOp = async (basis = 1, dataset: ArbitrageSet, log: Boolean): Promise<
     await addCPrices(tickers, 'USD', 'JPY');
     tickers = await addBPrices(tickers, bb, symbols.map(el => el + '/JPY'), 'USD');
     const dataset = expectedReturn(tickers, arbitrageConfig);
+    logger(dataset);
     const data = await judgeOp(1, dataset, true);
     console.log('data :>> ', data);
 
